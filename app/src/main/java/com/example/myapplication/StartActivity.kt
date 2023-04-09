400666d610ec79563e7cf7bbef484ef0ef10b0ba
Anatoly Kushnarenko <Anatoly.kushnarenko@gmail.com>
2023-04-09 20:15:13 +0300

    Добавлен выход из приложения по кнопке "Выход"

--- app/src/main/java/com/example/myapplication/StartActivity.kt
+++ app/src/main/java/com/example/myapplication/StartActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -9,6 +10,9 @@ class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        val exitButton = findViewById<Button>(R.id.exit_btn)
+        exitButton.setOnClickListener {finishAffinity()}
     }
 
 }
