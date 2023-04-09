28d1f4c7e67b9aab3f1dcd3f9f0e7dba638beff7
Anatoly Kushnarenko <Anatoly.kushnarenko@gmail.com>
2023-04-09 20:09:58 +0300

    Теперь запускается в начале StartActivity

--- app/src/main/java/com/example/myapplication/StartActivity.kt
+++ app/src/main/java/com/example/myapplication/StartActivity.kt
@@ -1,11 +1,14 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
     }
+
 }
